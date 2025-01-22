ee26745bf6e94c0b093770b2d10e4abba76d819b
Naveen K <81550751+Naveen-1994@users.noreply.github.com>
2025-01-22 12:45:45 +0530

    click on entire whatsapp block

--- src/components/Contactme/Whatsapp.jsx
+++ src/components/Contactme/Whatsapp.jsx
@@ -5,8 +5,9 @@ import { RiWhatsappFill } from "react-icons/ri";
 const Whatsapp = () => {
     return (
         <div className='whatsapp-contact-block'>
+            <a href='https://wa.me/918105504812?text=Hello%20Naveen!%20I%27m%20interested%20in%20discussing%20a%20front-end%20development%20project.%20Can%20we%20talk%3F' target='_blank' rel='noreferrer' className='whatsapp-link'>
             <p className='whatsapp-title'>WhatsApp me</p>
-            <a href='https://wa.me/918105504812?text=Hello%20Naveen!%20I%27m%20interested%20in%20discussing%20a%20front-end%20development%20project.%20Can%20we%20talk%3F' target='_blank' rel='noreferrer' className='whatsapp-link'><RiWhatsappFill /></a>
+            <RiWhatsappFill /></a>
         </div>
     )
 }
